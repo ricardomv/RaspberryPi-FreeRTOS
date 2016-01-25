7401a1769682ae0f6d229b497484bfc20026fa9f
Jared Hull <programmerjared@yahoo.com>
2016-01-24 18:40:30 -0600

    Removed annoying -Wall
    No more annoying warnings.

--- dbuild.config.mk
+++ dbuild.config.mk
@@ -1,5 +1,5 @@
 
-CFLAGS += -march=armv7-a -g -Wall -Wextra
+CFLAGS += -march=armv7-a -g -Wextra
 CFLAGS += -I $(BASE)FreeRTOS/Source/portable/GCC/RaspberryPi/
 CFLAGS += -I $(BASE)FreeRTOS/Source/include/
 CFLAGS += -I $(BASE)Demo/Drivers/
