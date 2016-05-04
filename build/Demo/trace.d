001f5ada655fdd4a0364b244061a3c8013482a1c
Forty-Tw0 <jxh130030@utdallas.edu>
2016-05-04 14:14:53 -0500

    final3

--- build/Demo/trace.d
+++
@@ -1,5 +0,0 @@
-/home/ljosalfur/Desktop/rpi/RaspberryPi-FreeRTOS/build/Demo/trace.o: \
- /home/ljosalfur/Desktop/rpi/RaspberryPi-FreeRTOS/Demo/trace.c \
- /home/ljosalfur/Desktop/rpi/RaspberryPi-FreeRTOS/Drivers/video.h
-
-/home/ljosalfur/Desktop/rpi/RaspberryPi-FreeRTOS/Drivers/video.h:
--- build/Drivers/gpio.d
+++
@@ -1,5 +0,0 @@
-/home/ljosalfur/Desktop/rpi/RaspberryPi-FreeRTOS/build/Drivers/gpio.o: \
- /home/ljosalfur/Desktop/rpi/RaspberryPi-FreeRTOS/Drivers/gpio.c \
- /home/ljosalfur/Desktop/rpi/RaspberryPi-FreeRTOS/Drivers/gpio.h
-
-/home/ljosalfur/Desktop/rpi/RaspberryPi-FreeRTOS/Drivers/gpio.h:
